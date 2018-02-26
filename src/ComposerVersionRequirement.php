b060d4040fcf5ab0a727e0462a38d78e215b4df1
Andrew Berry <deviantintegral@gmail.com>
2018-02-26 09:33:20 -0500

    Fix not updating content-hash

--- src/ComposerVersionRequirement.php
+++ src/ComposerVersionRequirement.php
@@ -8,6 +8,7 @@ use Composer\Factory;
 use Composer\IO\IOInterface;
 use Composer\Json\JsonFile;
 use Composer\Json\JsonManipulator;
+use Composer\Package\Locker;
 use Composer\Plugin\PluginInterface;
 use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
@@ -111,6 +112,12 @@ class ComposerVersionRequirement implements PluginInterface, EventSubscriberInte
     $manipulator->addProperty('extra.composer-version', $constraint);
 
     file_put_contents($json->getPath(), $manipulator->getContents());
+    $contents = file_get_contents($json->getPath());
+    $lockFile = "json" === pathinfo($file, PATHINFO_EXTENSION)
+      ? substr($file, 0, -4).'lock'
+      : $file . '.lock';
+    $locker = new Locker($this->io, new JsonFile($lockFile, null, $this->io), $this->composer->getRepositoryManager(), $this->composer->getInstallationManager(), $contents);
+    $this->composer->setLocker($locker);
 
     $this->io->writeError(sprintf('<info>Composer requirement set to %s.</info>', $constraint));
   }
